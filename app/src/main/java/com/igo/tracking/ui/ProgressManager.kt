3ab8824353daf2506168966218c72f15ead5d1f9
koassumo <koassumo@gmail.com>
2024-02-15 12:39:29 +0100

    2 coroutines in PyrolysisFragment

--- app/src/main/java/com/igo/tracking/ui/ProgressManager.kt
+++ app/src/main/java/com/igo/tracking/ui/ProgressManager.kt
@@ -3,7 +3,11 @@ import kotlinx.coroutines.*
 
 object ProgressManager {
     private var job: Job? = null
+    private var job2: Job? = null
     private var currentProgress: Int = 0
+    private var currentProgress2: Int = 0
+    private var onCompleteListener: (() -> Unit)? = null
+    private var onCompleteListener2: (() -> Unit)? = null
 
     fun startProgress(updateProgress: (Int) -> Unit) {
         job = CoroutineScope(Dispatchers.Main).launch {
@@ -13,15 +17,51 @@ object ProgressManager {
                     withContext(Dispatchers.Main) {
                         updateProgress(i)
                     }
-                    delay(600)
+                    delay(100)
                 }
+                onCompleteListener?.invoke()
             }
         }
     }
 
+    fun startProgress2(updateProgress: (Int) -> Unit) {
+        job2 = CoroutineScope(Dispatchers.Main).launch {
+            withContext(Dispatchers.Default) {
+                for (i in currentProgress2..100) {
+                    currentProgress2 = i
+                    withContext(Dispatchers.Main) {
+                        updateProgress(i)
+                    }
+                    delay(100)
+                }
+                onCompleteListener2?.invoke()
+            }
+        }
+    }
+    fun setOnCompleteListener(listener: () -> Unit) {
+        onCompleteListener = listener
+    }
+    fun setOnCompleteListener2 (listener: () -> Unit) {
+        onCompleteListener2 = listener
+    }
+    fun removeOnCompleteListener() {
+        onCompleteListener = null
+    }
+
+   fun removeOnCompleteListener2() {
+        onCompleteListener2 = null
+    }
+
+
+
+
 
     fun getCurrentProgress(): Int {
         return currentProgress
+    }
+
+    fun getCurrentProgress2(): Int {
+        return currentProgress2
     }
 
     fun pauseProgress() {
--- app/src/main/java/com/igo/tracking/ui/pyrolysis/PyrolysisFragment.kt
+++ app/src/main/java/com/igo/tracking/ui/pyrolysis/PyrolysisFragment.kt
@@ -32,20 +32,46 @@ class PyrolysisFragment : Fragment() {
 //        }
         val progressBar = binding.pyrProgress1
         val textView = binding.pyrProgressTxtValue1
+        val progressBar2 = binding.pyrProgress2
+        val textView2 = binding.pyrProgressTxtValue2
 
         binding.pyrPyrProgress1Btn.setOnClickListener {
             ProgressManager.startProgress { progress ->
                 progressBar.progress = progress
                 textView.text = "$progress%"
             }
+            ProgressManager.setOnCompleteListener {
+                activity?.runOnUiThread {
+                    textView.text = "ок"
+                }
+            }
         }
 
-        // Если есть сохраненное состояние прогресса, продолжаем с него
-        val savedProgress = ProgressManager.getCurrentProgress()
-        if (savedProgress > 0) {
-            progressBar.progress = savedProgress
-            textView.text = "$savedProgress%"
+        binding.pyrPyrProgress2Btn.setOnClickListener {
+            ProgressManager.startProgress2 { progress ->
+                progressBar2.progress = progress
+                textView2.text = "$progress%"
+            }
+            ProgressManager.setOnCompleteListener2 {
+                activity?.runOnUiThread {
+                    textView2.text = "ок"
+                }
+            }
         }
+
+//        // Если есть сохраненное состояние прогресса, продолжаем с него
+//        val savedProgress = ProgressManager.getCurrentProgress()
+//        if (savedProgress > 0) {
+//            progressBar.progress = savedProgress
+//            textView.text = "$savedProgress%"
+//        }
+//
+//        val savedProgress2 = ProgressManager.getCurrentProgress2()
+//        if (savedProgress2 > 0) {
+//            progressBar2.progress = savedProgress
+//            textView2.text = "$savedProgress%"
+//        }
+
 
         return root
     }
@@ -61,7 +87,12 @@ class PyrolysisFragment : Fragment() {
             //binding.pyrProgress1.progress = savedProgress
             //binding.pyrProgressTxtValue1.text = "$savedProgress%"
         }
-
+        val savedProgress2 = ProgressManager.getCurrentProgress2()
+        if (savedProgress2 > 0) {
+            binding.pyrPyrProgress2Btn.performClick()
+            //binding.pyrProgress1.progress = savedProgress
+            //binding.pyrProgressTxtValue1.text = "$savedProgress%"
+        }
     }
 //
 //    override fun onPause() {
